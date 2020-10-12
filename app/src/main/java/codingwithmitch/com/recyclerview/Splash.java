ae6ccbffed1cb28ef70c47bfcf17f141e4b2149a
Nikola Pavlovic <npavlovic12@gmail.com>
2020-10-12 00:09:29 -0400

    final push master detail

--- app/src/main/java/codingwithmitch/com/recyclerview/Splash.java
+++ app/src/main/java/codingwithmitch/com/recyclerview/Splash.java
@@ -27,7 +27,7 @@ public class Splash extends AppCompatActivity {
             }
         };
         Timer opening = new Timer();
-        opening.schedule(task,8000);
+        opening.schedule(task,6000);
 
 
 
