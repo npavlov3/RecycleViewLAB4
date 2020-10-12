b2fe928257497c2ed401ef5b8ef376b1b0da3e42
Nikola Pavlovic <npavlovic12@gmail.com>
2020-10-11 20:57:28 -0400

    final push master detail

--- app/src/main/java/codingwithmitch/com/recyclerview/Splash.java
+++ app/src/main/java/codingwithmitch/com/recyclerview/Splash.java
@@ -1,6 +1,7 @@
 package codingwithmitch.com.recyclerview;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,19 +10,24 @@ import java.util.TimerTask;
 
 public class Splash extends AppCompatActivity {
 
+    MediaPlayer splash;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash2);
+        splash = new MediaPlayer();
+        splash = MediaPlayer.create(this, R.raw.splash);
+        splash.start();
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
                 finish();
+                splash.pause();
                 startActivity(new Intent(Splash.this, MainActivity.class));
             }
         };
         Timer opening = new Timer();
-        opening.schedule(task,5000);
+        opening.schedule(task,8000);
 
 
 
